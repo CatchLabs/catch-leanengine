785c456fc3a790b0b352c9e483a2fd29357b9166
Zeno Zeng <zenoofzeng@gmail.com>
2016-05-24 23:08:35 +0000

    2.0.0-alpha.0

--- index.js
+++ index.js
@@ -1,11 +1,2 @@
-var path = require('path');
-var projectRoot = path.resolve(path.dirname(__filename), '../..');
-
-function cloudRequire(modulePath) {
-    return require(path.join(projectRoot, modulePath));
-}
-
-module.exports = {
-    require: cloudRequire,
-    AV: cloudRequire('cloud/ng/engine')
-};
+var AV = require('leanengine');
+module.exports = AV;
